0e915759db0f9558c6df9e4a299621f988f361cf
Felix Huber <felix.a.huber@gmx.net>
2021-07-08 16:07:35 +0200

    Cleanup and adding of an readme

--- src/resources/js/bootstrap.js
+++ src/resources/js/bootstrap.js
@@ -20,26 +20,6 @@ import Echo from 'laravel-echo';
 
 window.Pusher = require('pusher-js');
 
-/*window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    forceTLS: true
-});*/
-
-/*
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'your-pusher-key',
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    forceTLS: false,
-    disableStats: true,
-});
-*/
-
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
